9a57467662d6715bf4981b21ec95677330670abe
rachurimahesh <46364713+rachurimahesh@users.noreply.github.com>
2019-01-04 08:31:37 +0000

    Update FilmFlixApplication.java

--- src/com/sceneca/flimflix/FilmFlixApplication.java
+++ src/com/sceneca/flimflix/FilmFlixApplication.java
@@ -3,11 +3,17 @@ package com.sceneca.flimflix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application which will boot up and run spring application
+ * 
+ * @author Mahesh Rachuri
+ *
+ */
 @SpringBootApplication
 public class FilmFlixApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FilmFlixApplication.class, args);	
+		SpringApplication.run(FilmFlixApplication.class, args);
 
 	}
 }
